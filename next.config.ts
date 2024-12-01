9d201f15244cc43124e7c977098e2aef26114073
aidqen <idanmarkin8@gmail.com>
2024-12-01 22:46:39 +0200

    Add Provider for Redux

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig = {
   experimental: {
     reactRoot: true,
+    appDir: true,
   },
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
+
 import type { Metadata } from 'next'
 import './globals.css'
-import { AppHeader } from './components/AppHeader'
+import { AppHeader } from '../components/AppHeader'
 
 import '@fontsource-variable/montserrat';
-import { Sidebar } from './components/Sidebar';
+import { Sidebar } from '@/components/Sidebar';
+import { ReduxProvider } from '@/components/ReactProvider';
 
 export const metadata: Metadata = {
   title: 'Daily Planner',
@@ -18,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="w-full">
-        {/* <Sidebar /> */}
-        <AppHeader />
-        <main className='p-4 pt-10'>{children}</main>
+        <ReduxProvider>
+          <Sidebar />
+          <AppHeader />
+          <main className="p-4 pt-10">{children}</main>
+        </ReduxProvider>
       </body>
     </html>
   )
